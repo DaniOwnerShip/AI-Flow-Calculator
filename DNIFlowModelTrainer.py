28a16580d82b337803fcdd0638e0074ec47d5afc
DaniOwnerShip <daniorcha@hotmail.com>
2023-05-04 20:51:34 +0200

    multiple input model

--- DNIFlowModelTrainer.py
+++ DNIFlowModelTrainer.py
@@ -2,18 +2,14 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import pandas as pd
-
-#from sklearn.preprocessing import MinMaxScaler
-#from sklearn.preprocessing import StandardScaler
-
+ 
 # data csv columns:  Flow	PressureIN	DNIraw	DNIreal	TIN	TOUT	DeltaT	JP_Fx 
 DATA = pd.read_csv('./dataCS.csv', delimiter=';', na_values=['NaN'])
 
 # predict value
 FLOW_SF =  DATA['Flow'].astype(float).values.reshape(-1, 1) 
 
-# predictor values 
-# for test, change manually to "inputData" and "weight" 
+# predictor values  
 PREASSURE_IN =  DATA['PressureIN'].astype(float).values 
 DNI_RAW =  DATA['DNIraw'].astype(float).values 
 DNI_RAW = np.clip(DNI_RAW, a_min=0, a_max=None)
@@ -23,59 +19,52 @@ T_OUT =  DATA['TOUT'].astype(float).values
 DELTA_T =  DATA['DeltaT'].astype(float).values
 DELTA_T = np.clip(DELTA_T, a_min=0, a_max=None)
 JP_FX =  DATA['JP_Fx'].astype(float).values   
-
-##################################################################
-# Change values to TRAIN MODEL:  
  
-inputDataTrain = DATA[['DNIreal', 'TOUT', 'JP_Fx', 'DeltaT', 'PressureIN']]  
 
- 
-# Change value <T_OUT> to TRAIN MODEL: 
-weightTrain =  JP_FX 
+##################################################################  
 
-# Change values to TEST MODEL:
-AtestData = ['797.36', '387.44', '7.07', '93.4', '28.27'] 
-AtestData = np.array([AtestData])  # Convertir la lista a una matriz de numpy con una sola fila
-AtestData = AtestData.astype(float)
+#Change name of rows to test model
+inputDataTrain = DATA[['DNIreal', 'TOUT', 'JP_Fx', 'DeltaT', 'PressureIN']]    
+
+#select the column of Data Test file to test. must be the same order like Data Train
+#in this case Test data are data of Train data:
+
+rowTest = 356 # <=== CHANGE THIS VALUE  
+
+rowTest = rowTest + 2 #add 2 row (header + start 1) to match row of ./dataCS.csv
+
+# print data
+print(inputDataTrain.shape)
+print(inputDataTrain.iloc[rowTest].values)
+  
+dataTest = inputDataTrain.iloc[rowTest].to_numpy().astype(float).reshape(1, -1)
+
+#weight variable
+weightTrain =  JP_FX # no effect = None
  
 ##################################################################
     
+
 # instance model 
 model = LinearRegression()   
 
-# train model
-model.fit(inputDataTrain, FLOW_SF, sample_weight = weightTrain) 
- 
-# calculate result 2D
-result = model.predict(AtestData)
+# train modelmodel = LinearRegression() 
+model.fit(inputDataTrain.values, FLOW_SF, sample_weight=weightTrain)
+
+
+# calculate result   
+result = model.predict(dataTest)
 
  
 # print results
 print('***************') 
 print('MODEL OUTPUT: ', result)  
+print('---------------') 
 print('coef :', model.coef_)
 print('intercept :', model.intercept_)   
 print('***************') 
-  
-"""
-# Graphic plot
-plt.scatter(inputDataTrain, FLOW_SF, color='blue')
-plt.plot(inputDataTrain, model.predict(inputDataTrain), color='red', linewidth=3)
-plt.title('Caudal vs Radiación')
-plt.xlabel('radiación')
-plt.ylabel('caudal')
-plt.show() 
 
 
 
 
-#Save and load model trained functions
-
-with open('modelo_entrenado.pkl', 'rb') as archivo:
-    modelo = pickle.load(archivo)
-"""
-
-""""
-with open('modelo_entrenado.pkl', 'wb') as archivo:
-    pickle.dump(modelo, archivo)
-"""
+ 
